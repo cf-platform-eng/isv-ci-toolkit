43ab59f608758a5e93422a638e49d569159e142c
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2019-06-25 16:06:37 -0400

    WIP: use hosted copy of upload script

--- examples/install-uninstall-pas-tile/scripts/pas-test.sh
+++ examples/install-uninstall-pas-tile/scripts/pas-test.sh
@@ -38,7 +38,7 @@ PRODUCT_VERSION=$(tileinspect metadata -t /tile/$TILE_NAME | yq -r .product_vers
 om -k upload-product -p /tile/${TILE_NAME}
 om -k stage-product --product-name ${PRODUCT_NAME} --product-version ${PRODUCT_VERSION}
 
-/scripts/upload_and_assign_stemcells.sh $(om -k curl -s -p /api/v0/stemcell_assignments | jq -r .stemcell_library[0].infrastructure)
+upload_and_assign_stemcells.sh $(om -k curl -s -p /api/v0/stemcell_assignments | jq -r .stemcell_library[0].infrastructure)
 
 build_tile_config
 
