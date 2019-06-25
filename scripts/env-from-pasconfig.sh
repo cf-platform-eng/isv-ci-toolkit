2b1d59b246cc753a9d986b67c41dd9ae6cf2944e
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2019-06-25 15:54:58 -0400

    pas config helper shouldn't close the terminal
    Co-authored-by: Ernie Billing <ebilling@pivotal.io>

--- scripts/env-from-pasconfig.sh
+++ scripts/env-from-pasconfig.sh
@@ -3,13 +3,18 @@ set +x # Hide secrets
 
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && echo -e "You must source this script\nsource ${0}" && exit 1
 
+
+run() {
+    export OM_PASSWORD=$(cat ${configjson} | jq -r .ops_manager.password)
+    export OM_TARGET=$(cat ${configjson} | jq -r .ops_manager.url)
+    export OM_USERNAME=$(cat ${configjson} | jq -r .ops_manager.username)
+}
+
 configjson=$1
 if [[ -z "${configjson}" ]] ; then
     echo "USAGE: $0 <config json file>"
     echo "  config json file: Environment config file from toolsmiths"
-    exit 1
+else
+    run
 fi
 
-export OM_PASSWORD=$(cat ${configjson} | jq -r .ops_manager.password)
-export OM_TARGET=$(cat ${configjson} | jq -r .ops_manager.url)
-export OM_USERNAME=$(cat ${configjson} | jq -r .ops_manager.username)
