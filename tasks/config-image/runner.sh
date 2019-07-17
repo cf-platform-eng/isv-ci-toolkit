a1077867638315ed681443bc056c785b09ea8212
Todd Ritchie <tritchie+github@pivotal.io>
2019-07-17 11:02:17 -0400

    Config SRT on GCP (#14)
    Given an environment.json file and a credentials.json file, this docker image will configure the director and upload+stage+configure the SRT tile as well. 
    
    Now supports both Azure and GCP.

---
+++ tasks/config-image/runner.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -xe
+
+iaas=$(jq -r '.iaas' /input/environment.json)
+
+# Configure OM environment variables
+. ./setup_om.sh /input/environment.json /input/credentials.json
+
+# Set up PivNet token
+export PIVNET_TOKEN="$(jq -r '.pivnet_token' /input/credentials.json)"
+
+# Set up IDP
+PASSWORD="$(jq -r '.password' /input/credentials.json)"
+om -k configure-authentication --decryption-passphrase "$PASSWORD"
+
+# Configure the director
+./build_configure_bosh_json.sh /input/environment.json /input/credentials.json > /tmp/director-config.json
+om -k configure-director --config /tmp/director-config.json
+
+if [[ "${SKIP_TILE_UPLOAD}" != "true" ]] ; then
+    marman download-tile -s elastic-runtime -v ${PRODUCT_VERSION} -f "${PRODUCT_NAME}-.*.pivotal"
+    om -k upload-product --product ./*.pivotal
+    om -k stage-product --product-name cf --product-version ${PRODUCT_VERSION}
+fi
+
+# Configure
+./build_configure_product_json.sh /input/environment.json "/input/elastic-runtime.srt.${iaas}.json" $PCF_VERSION $PRODUCT_VERSION > /tmp/product-config.json
+cat /tmp/product-config.json | jq '. + {"resource-config": .resource_config} | del(.resource_config) + {"network-properties": .network} | del(.network) + {"product-properties": .properties} | del(.properties)' > /tmp/product-config-new-schema.json
+om -k configure-product --config /tmp/product-config-new-schema.json --ops-file /input/add-cf.yml
+
+if [ "${iaas}" = "gcp" ] ; then
+    ./upload_and_assign_stemcells.sh google
+else
+    ./upload_and_assign_stemcells.sh "${iaas}"
+fi
