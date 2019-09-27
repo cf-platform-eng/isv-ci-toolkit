57c63a0a3cdbe23344c6dc5165e328eda90a7707
Todd Ritchie <tritchie+github@pivotal.io>
2019-09-27 14:36:27 -0400

    Consolidate duplicated scripts from the config images to the tools image (#30)

--- tasks/config-and-upgrade-product-image/run.sh
+++ tasks/config-and-upgrade-product-image/run.sh
@@ -6,8 +6,8 @@ needs check
 
 iaas=$(jq -r '.iaas' /input/environment.json)
 
-# Configure OM environment variables
-UPGRADE=true . ./setup_om.sh /input/environment.json /input/credentials.json
+# shellcheck disable=SC1091
+UPGRADE=true . setup_om.sh /input/environment.json /input/credentials.json
 
 # Set up PivNet token
 PIVNET_TOKEN="$(jq -r '.pivnet_token' /input/credentials.json)"
@@ -18,7 +18,7 @@ PASSWORD="$(jq -r '.password' /input/credentials.json)"
 om -k configure-authentication --decryption-passphrase "$PASSWORD"
 
 # Configure the director
-./build_configure_bosh_json.sh /input/environment.json /input/credentials.json > /tmp/director-config.json
+build_configure_bosh_json.sh /input/environment.json /input/credentials.json > /tmp/director-config.json
 om -k configure-director --config /tmp/director-config.json
 
 if [[ "${SKIP_TILE_UPLOAD}" != "true" ]] ; then
@@ -29,14 +29,14 @@ if [[ "${SKIP_TILE_UPLOAD}" != "true" ]] ; then
 fi
 
 # Configure
-./build_configure_product_json.sh /input/environment.json "/input/elastic-runtime.srt.${iaas}.json" "$STARTING_PCF_VERSION" "$PRODUCT_VERSION" > /tmp/product-config.json
+build_configure_product_json.sh /input/environment.json "/input/elastic-runtime.srt.${iaas}.json" "$STARTING_PCF_VERSION" "$PRODUCT_VERSION" > /tmp/product-config.json
 jq '. + {"resource-config": .resource_config} | del(.resource_config) + {"network-properties": .network} | del(.network) + {"product-properties": .properties} | del(.properties)' /tmp/product-config.json > /tmp/product-config-new-schema.json
 om -k configure-product --config /tmp/product-config-new-schema.json --ops-file /input/add-cf.yml
 
 if [ "${iaas}" = "gcp" ] ; then
-    ./upload_and_assign_stemcells.sh google
+    upload_and_assign_stemcells.sh google
 else
-    ./upload_and_assign_stemcells.sh "${iaas}"
+    upload_and_assign_stemcells.sh "${iaas}"
 fi
 
 # Deploy
@@ -46,7 +46,8 @@ fi
 
 om -k export-installation -o /tmp/installation.zip
 
-. ./setup_om.sh /input/environment.json /input/credentials.json
+# shellcheck disable=SC1091
+. setup_om.sh /input/environment.json /input/credentials.json
 
 om -k -d "$OM_PASSWORD" import-installation -i /tmp/installation.zip
 
@@ -58,14 +59,14 @@ if [[ "${SKIP_TILE_UPLOAD}" != "true" ]] ; then
 fi
 
 # Configure
-./build_configure_product_json.sh /input/environment.json "/input/elastic-runtime.srt.${iaas}.json" "$UPGRADED_PCF_VERSION" "$UPGRADED_PRODUCT_VERSION" > /tmp/product-config.json
+build_configure_product_json.sh /input/environment.json "/input/elastic-runtime.srt.${iaas}.json" "$UPGRADED_PCF_VERSION" "$UPGRADED_PRODUCT_VERSION" > /tmp/product-config.json
 jq '. + {"resource-config": .resource_config} | del(.resource_config) + {"network-properties": .network} | del(.network) + {"product-properties": .properties} | del(.properties)' /tmp/product-config.json > /tmp/product-config-new-schema.json
 om -k configure-product --config /tmp/product-config-new-schema.json --ops-file /input/add-cf.yml
 
 if [ "${iaas}" = "gcp" ] ; then
-    ./upload_and_assign_stemcells.sh google
+    upload_and_assign_stemcells.sh google
 else
-    ./upload_and_assign_stemcells.sh "${iaas}"
+    upload_and_assign_stemcells.sh "${iaas}"
 fi
 
 # Deploy
