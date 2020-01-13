37544bdbc786b93384a720e3a05f902112812580
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2020-01-13 15:58:48 -0500

    Introduce new entrypoint standard with jeb.sh
    - Support the CN-JEB entrypoint spec
      - run calls run.sh in the WORKDIR
      - [list-]needs calls needs in the WORKDIR
      - shell run's the image's shell
    
    [#170633520]
    
    Co-authored-by: Todd Ritchie <tritchie+github@pivotal.io>
    Co-authored-by: Pete Wall <pwall@pivotal.io>

---
+++ base-image/jeb.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+command=${1:-run}
+
+usage() {
+  echo "CN-JEB entrypoint standard"
+  echo ""
+  echo "Commands are:"
+  echo "  help"
+  echo "  run"
+  echo "  shell"
+  echo "  needs"
+  echo "  list-needs"
+
+}
+
+case "${command}" in
+  run)
+    run.sh
+    ;;
+  *needs)
+    needs list
+    ;;
+  shell)
+    $SHELL
+    ;;
+  *)
+    usage
+    ;;
+esac
